5449a9cd42ea3acd892b3eaa7ca64fea11bfd4c8
Sebastian <Sebastian@gmail.com>
2026-04-09 15:39:11 -0500

    subiendo ejer 1 en c#

---
+++ SE3-C/ejer1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SE3_C
+{
+    internal class ejer1
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera;
+
+            Console.Write("Ingresa tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingresa tu carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            Console.WriteLine("{0}, Bienvenido al curso de Fundamentos de Algoritmos de la carrera {1}", nombre, carrera);
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
